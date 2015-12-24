10a591d4cc6304cecf4473c44e615cce54ba184a
Vincss <Vincent@ViPC>
2015-12-24 11:52:01 +0100

    add logs

--- sleepingServerStarter.js
+++ sleepingServerStarter.js
@@ -2,33 +2,72 @@
 // var packageSleep = JSON.parse(require('fs').readFileSync('package.json',
 // 'utf8'));
 
+var fileSystem = require('fs');
 var settings = require('js-yaml').load(
-		require('fs').readFileSync('sleepingSettings.yml'));
+		fileSystem.readFileSync('sleepingSettings.yml'));
 
 var mc = require('minecraft-protocol');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var childProcess = require('child_process');
 
+var winston = require('winston');
+var logFolder = 'log/'
+var dateFormat = require('dateformat');
+var logger;
+
 var webServer;
 var mcServer;
 
 var startMinecraft = function() {
-	console.log('----------- Starting Minecraft : ' + settings.minecraftCommand
+	logger.info('----------- Starting Minecraft : ' + settings.minecraftCommand
 			+ ' ----------- ');
 
-	//settings.minecraftCommand = 'notepad';
+	// settings.minecraftCommand = 'notepad';
 	var mcProcess = childProcess.execSync(settings.minecraftCommand, {
 		stdio : "inherit"
 	});
-	console.log('----------- Minecraft stopped -----------');
+	logger.info('----------- Minecraft stopped -----------');
+};
+
+var getDate = function() {
+	return dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss.l');
+};
+
+process.on('uncaughtException', function(err) {
+	logger.info('Caught exception: ' + err);
+	logger.info('...Exiting..');
+	process.exit(1);
+});
+
+var initLog = function() {
+	if (!fileSystem.existsSync(logFolder)) {
+		fileSystem.mkdirSync(logFolder);
+	}
+
+	logger = new winston.Logger({
+		level : 'info',
+		transports : [ new (winston.transports.Console)({
+			timestamp : getDate,
+			colorize : true
+		}), new (winston.transports.File)({
+			filename : logFolder + 'sleepinServer.log',
+			timestamp : getDate,
+			maxsize : 2048,
+			maxFiles : 3,
+			json : false
+		}) ]
+	});
 };
 
 var init = function() {
+
+	initLog();
+
 	if (settings.webPort > 0) {
 		webServer = connect().use(serveStatic(settings.webDir)).listen(
 				settings.webPort);
-		console.log("Starting web server on *:" + settings.webPort
+		logger.info("Starting web server on *:" + settings.webPort
 				+ " webDir: " + settings.webDir);
 	}
 
@@ -39,7 +78,7 @@ var init = function() {
 		motd : settings.serverName,
 		port : settings.serverPort, // optional
 	});
-	console.log('Waiting for a Prince to come. [' + settings.serverPort
+	logger.info('Waiting for a Prince to come. [' + settings.serverPort
 			+ '] Or someone to hit a key.');
 
 	process.stdin.resume();
@@ -58,7 +97,7 @@ var init = function() {
 			reducedDebugInfo : false
 		});
 
-		console.log('Prince has come, time to wake up.')
+		logger.info('Prince has come, time to wake up.')
 
 		client.end(settings.loginMessage);
 		closeServer();
@@ -68,7 +107,7 @@ var init = function() {
 init();
 
 var closeServer = function() {
-	console.log('Cleaning up the place.')
+	logger.info('Cleaning up the place.')
 	mcServer.close();
 	if (webServer !== undefined)
 		webServer.close();
@@ -76,9 +115,9 @@ var closeServer = function() {
 	if (settings.startMinecraft > 0) {
 		startMinecraft();
 
-		console.log('...Time to kill me if you want...')
+		logger.info('...Time to kill me if you want...')
 		setTimeout(function() {
-			console.log('...Too late !...');
+			logger.info('...Too late !...');
 			init();
 		}, 5000); // restart server
 	}
