d8ca7ead4a431df10bc509a58a49740ca847f830
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 08:32:04 +0100

    update version

--- sleepingSettings.js
+++ sleepingSettings.js
@@ -14,7 +14,7 @@ const DefaultSettings = {
 
     startMinecraft: 1,				// 0 to disable
     minecraftCommand: 'java -jar spigot.jar nogui',
-    version: '1.16.3'
+    version: '1.16.4'
 };
 
 function saveDefault() {
