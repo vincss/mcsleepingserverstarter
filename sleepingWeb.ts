d049024ce79f6da4682b7ea5ebdb62d4ac14cfa0
vincss <vincss@gmail.com>
2021-04-02 15:45:28 +0000

    WIP web-ui

--- sleepingWeb.ts
+++ sleepingWeb.ts
@@ -1,8 +1,9 @@
 import express, { Express } from 'express';
-import exphbs  from 'express-handlebars';
+import exphbs from 'express-handlebars';
+import * as http from 'http';
 
 import { ISleepingServer } from "./sleepingServerInterface";
-import { Settings } from "./sleepingSettings";
+import { DefaultFavIconString, Settings } from "./sleepingSettings";
 import { getLogger, LoggerType } from './sleepingLogger';
 
 export class SleepingWeb implements ISleepingServer {
@@ -10,6 +11,7 @@ export class SleepingWeb implements ISleepingServer {
   logger: LoggerType;
   playerConnectionCallBack: () => void;
   app: Express;
+  server?: http.Server;
 
   constructor(settings: Settings, playerConnectionCallBack: () => void) {
     this.settings = settings;
@@ -22,28 +24,41 @@ export class SleepingWeb implements ISleepingServer {
 
     this.app.engine('hbs', exphbs({
       defaultLayout: 'main',
-      extname: '.hbs'
-  }));
-  
-  this.app.set('view engine', 'hbs');
+      extname: '.hbs',
+      helpers: {
+        title: () => { return this.settings.serverName },
+        favIcon: () => { return this.settings.favIcon || DefaultFavIconString },
+      }
+    }));
 
-    this.app.get("/", (req, res) => {
-      res.render('home');
+    this.app.set('view engine', 'hbs');
+    this.app.use(express.static('views'));
+
+    this.app.get('/', (req, res) => {
+      res.render('home', { message: this.settings.loginMessage });
+    });
+
+    this.app.post('/wakeup', (req, res) => {
+      this.playerConnectionCallBack();
+      res.send('received');
     })
 
-    this.app.listen(this.settings.webPort, () => {
+    this.server = this.app.listen(this.settings.webPort, () => {
       this.logger.info(`Starting web server on *:${this.settings.webPort} webDir: ${this.settings.webDir}`);
     })
 
     /* 
     connect().use(serveStatic(this.settings.webDir)).listen(
         this.settings.webPort);
-    this.logger.info(`Starting web server on *:${this.settings.webPort} webDir: ${this.settings.webDir}`); */
+    this.logger.info(`Starting web server on *:${this.settings.webPort} webDir: ${this.settings.webDir}`); 
+    */
 
   };
 
   close = async () => {
-
+    if (this.server) {
+      this.server.close();
+    }
   };
 
 }
